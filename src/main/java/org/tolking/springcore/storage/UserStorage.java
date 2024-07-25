cc7c1772ed166e1d2a2d529f135f6ffb6079b56f
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-07-25 20:50:51 +0500

    FIX: checking across all users (Including Trainee and Trainer)

--- src/main/java/org/tolking/springcore/storage/UserStorage.java
+++ src/main/java/org/tolking/springcore/storage/UserStorage.java
@@ -6,20 +6,26 @@ import java.util.concurrent.ConcurrentHashMap;
 import static org.tolking.springcore.util.EntityMapperUtils.getUsername;
 
 public class UserStorage {
-    private final List<Map<String, String>> userRecords;
     private final Map<String, Integer> userCount = new HashMap<>();
 
     public UserStorage(ConcurrentHashMap<Long,Map<String, String>> anotherMap) {
-        userRecords = Collections.synchronizedList(new ArrayList<>(anotherMap.values()));
+        for (var record : anotherMap.values()){
+            String username = getUsername(record);
+
+            putCounter(username);
+        }
     }
 
     public int getSerialNumber(Map<String, String> record){
         String username = getUsername(record);
 
-        userRecords.add(record);
-        userCount.put(username, userCount.getOrDefault(username,-1) + 1);
+        putCounter(username);
 
         return userCount.get(username);
+    }
+
+    private void putCounter(String username) {
+        userCount.put(username, userCount.getOrDefault(username,-1) + 1);
     }
 
 
